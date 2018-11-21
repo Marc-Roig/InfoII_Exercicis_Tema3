3dc8c89e41c345ad470e29fbf3f7e6fb5c8a4d25
Marc Roig <marc12info@gmail.com>
2018-11-21 17:44:53 +0100

    Parcial I

---
+++ Solucio_Examen_InfoII_primer_parcial.cpp
@@ -0,0 +1,93 @@
+struct str_LLC_resp {
+	uchar datalen;
+	uchar* data;
+};
+
+// CAPA 2A - MAC
+class MAC() {
+public:
+
+	void SendFrm(uchar datalen, uchar* data);
+	bool isResponseOK();
+	void readResponse(uchar* datalen, uchar* data);
+
+};
+
+volatile MAC mac = MAC();
+
+// CAPA 2B - LLC
+class LLC() {
+
+public:
+	bool busy, ResponseAvailable, ResponseFailed;
+	str_LLC_resp messageToSend, response;
+	char nIntentos;
+
+	LLC() {
+
+		busy = false;
+		ResponseFailed = false;
+		ResponseAvailable = false;
+		nIntentos = 0;
+
+		SetTim(100, AttTimer, 1); // Cada 100ms, crida AttTimer, repeteix per sempre.
+
+	}
+
+	void SendFrm(uchar datalen, uchar* data, uchar num_reintentos) {
+
+		if (busy) return;
+
+		nIntentos = num_reintentos % 10 + 1; // Un intent mes tots els altres reintents 
+		messageToSend.datalen = datalen % 10;
+		messageToSend.data = data;
+
+		busy = true;
+
+	}
+
+	uchar getResponseStatus() {
+
+		if (busy || !ResponseAvailable) return 0;
+		else if (ResponseAvailable) return 1;
+		else return 2; // ResponseFailed = true
+
+	}
+
+	void ReadResponse(str_LLC_resp * r) {
+
+		if (!ResponseAvailable || ResponseFailed) return;
+
+		r->datalen = response.datalen;
+		for (int i = 0; i < response.datalen; i++) {
+			r->data[i] = response.data[i];
+		}
+
+		ResponseAvailable = false;
+
+	}
+
+};
+
+volatile LLC llc = LLC();
+
+void AttTimer() {
+
+	if (!llc.busy || llc.ResponseFailed) return;
+
+	if (mac.isResponseOK()) {
+
+		mac.SendFrm(llc.messageToSend.datalen, llc.messageToSend.data);
+		llc.ResponseFailed = false;
+		llc.ResponseAvailable = true;
+		llc.busy = false;
+		llc.nIntentos = 0;
+
+	} else {
+
+		llc.nIntentos--;
+		if (llc.nIntentos <= 0) llc.ResponseFailed = true; 
+
+	}
+
+}
